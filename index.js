a69576cecf5b23ab56dd36d465e69685d7715797
Cris Mihalache <me@f3rno.com>
2018-08-22 19:01:01 +0300

    (feature) add tests from bitfinex-api-node

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ module.exports = {
   nonce,
   genAuthSig,
   isSnapshot,
-  isClass,
+  isClass
 }
---
+++ test/lib/nonce.js
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+const nonce = require('../../lib/nonce')
+
+describe('nonce', () => {
+  it('is non-zero and a number', () => {
+    assert(!isNaN(nonce()) && nonce() > 0)
+  })
+
+  it('is increasing', () => {
+    assert(nonce() < nonce())
+  })
+})
