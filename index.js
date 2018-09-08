2e331374ee1cd4a9fc3ffc0cb90365d2e5b7fcd8
Cris Mihalache <me@f3rno.com>
2018-09-08 15:54:17 +0300

    (feature) add padCandles helper

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const nonce = require('./lib/nonce')
 const genAuthSig = require('./lib/gen_auth_sig')
 const isSnapshot = require('./lib/is_snapshot')
 const isClass = require('./lib/is_class')
+const padCandles = require('./lib/pad_candles')
 
 module.exports = {
   ...precision,
   nonce,
   genAuthSig,
   isSnapshot,
-  isClass
+  isClass,
+  padCandles
 }
