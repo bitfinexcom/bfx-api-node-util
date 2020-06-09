8ce862c17e9daa2df21732fcba842057897fe926
Cris Mihalache <me@f3rno.com>
2020-06-09 15:06:20 +0700

    (refactor) finalize PR

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const isSnapshot = require('./lib/is_snapshot')
 const isClass = require('./lib/is_class')
 const padCandles = require('./lib/pad_candles')
 
+/**
+ * This module contains minor utilities shared by the Bitfinex Node.JS API
+ * libraries.
+ *
+ * @license MIT
+ * @module bfx-api-node-util
+ */
+
 module.exports = {
   ...precision,
   nonce,
